73126d845b101894aad14a32e4028016fbcb1be7
ntwtsp <natthawattaemsupan@gmail.com>
2026-05-20 22:37:28 +0700

    feat: แสดงอาหาร/ตัว/วัน แทนพรีดิกวันคงเหลือ

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'chitnarong-farm-v4';
+const CACHE = 'chitnarong-farm-v5';
 const ASSETS = ['/Chitnarong-farm/', '/Chitnarong-farm/index.html'];
 
 self.addEventListener('install', e => {
